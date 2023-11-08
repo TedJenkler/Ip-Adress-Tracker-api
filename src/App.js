9d618fb283401019e755e48cdc455c48dd28751f
TedJenkler <Tedjenkler@gmail.com>
2023-11-08 06:35:44 -0200

    Logic work but you need to submit 2 times

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Map from './Components/Map';
 
 function App() {
 
-  const [ip, setIp] = useState("Loading...");
+  const [ip, setIp] = useState("");
   const [city, setCity] = useState("Loading...")
   const [country, setCountry] = useState("Loading...")
   const [postalcode, setPostalcode] = useState("Loading...")
@@ -14,25 +14,24 @@ function App() {
   const [isp, setISP] = useState("Loading...")
   const [coordinates, setCoordinates] = useState([18.79038, 98.98468])
 
-   //useEffect(() => {
-    //fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_WFzeE0kMyAo0pYv3HItVeZvoXdk97&ipAddress=${ip}`)
-      //.then((response) => response.json())
-      //.then((data) => {
-        //console.log(data)
-       // setIp(data.ip)
-        //setCity(data.location.city)
-        //setCountry(data.location.country)
-        //setPostalcode(data.location.postalCode)
-       // setTimezone(data.location.timezone)
-       // setISP(data.isp)
-       // setCoordinates([data.location.lat, data.location.lng])
-     // })
- // },[])
+  useEffect(() => {
+    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_WFzeE0kMyAo0pYv3HItVeZvoXdk97&ipAddress=${ip}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setIp(data.ip)
+        setCity(data.location.city)
+        setCountry(data.location.country)
+        setPostalcode(data.location.postalCode)
+        setTimezone(data.location.timezone)
+        setISP(data.isp)
+        setCoordinates([data.location.lat, data.location.lng])
+      })
+  },[])
   return (
   <main className='container'>
     <div className='row blue-background'>
       <h1 className='header'>IP Adress Tracker</h1>
-      <IPInput ip={ip} setIp={setIp} />
+      <IPInput ip={ip} setIp={setIp} setCity={setCity} setCountry={setCountry} setPostalcode={setPostalcode} setTimezone={setTimezone} setISP={setISP} />
       <DisplayValues ip={ip} city={city} country={country} postalcode={postalcode} timezone={timezone} isp={isp} />
     </div>
     <div className='row map-background'>
--- src/Components/IpInput.js
+++ src/Components/IpInput.js
@@ -1,6 +1,32 @@
+import { useState } from "react"
+import arrow from "../images/icon-arrow.svg"
+
 const IPInput = (props) => {
+    const [update, setUpdate] = useState("")
+    const handlechange = (e) => {
+        setUpdate(e.target.value)
+    }
+    const handleclick = async () => {
+
+        props.setIp(update)
+        try {
+            const data = await (await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_WFzeE0kMyAo0pYv3HItVeZvoXdk97&ipAddress=${props.ip}`)).json()
+            console.log(data)
+            props.setCity(data.location.city)
+            props.setCountry(data.location.country)
+            props.setPostalcode(data.location.postalCode)
+            props.setTimezone(data.location.timezone)
+            props.setISP(data.isp)
+            props.setCoordinates([data.location.lat, data.location.lng])
+        } catch (err) {
+            console.log("API seems to be down")
+        }
+    }
     return (<>
-    <input className="input" placeholder={props.ip}></input>
+    <div className="inputgroup">
+        <input className="input" onChange={handlechange} value={update} placeholder={props.ip}></input>
+        <button onClick={handleclick} className="submit"><img id="arrow" src={arrow} alt=""></img></button>
+    </div>
     </>)
 }
 
